73b02f44ce0cfa63bc7130d288251927ba7e1b6f
ESAMHADI <alshawshsam48@gmail.com>
2024-08-27 15:32:17 +0300

    this is my first project

---
+++ Untitled-1.js
@@ -0,0 +1,4 @@
+let name = "ESAM HSDI ADL "
+name.repeat()
+name.repeat(10)
+name
---
+++ nam.js
@@ -0,0 +1,12 @@
+// let name = 'esam hadi ali '
+// console.log(name.repeat())
+// console.log(name.repeat(4))
+// console.log(name.length)
+// console.log(name[0])
+// console.log(name[10])
+
+// for(let i=0 ;i<=10;i++){
+//     console.log("hello esam")
+//     console.log(i)
+//     console.log("esam",i)
+// }
